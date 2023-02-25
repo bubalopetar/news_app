804da8cd7d85a1a2d848fa902fbb63f1c10e4164
bubalopetar <bubalopetar95@gmail.com>
2023-02-25 22:41:50 +0100

    added ArticlesRepository class

---
+++ lib/features/articles/domain/repositories/articles_repository.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:news_app/core/error/failures.dart';
+import 'package:news_app/features/articles/domain/entities/article.dart';
+
+abstract class ArticlesRepository {
+  Future<Either<Failure, List<Article>>> getArticlesFrom({String url});
+}
