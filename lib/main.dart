127326d087576fa62642f49b6c017746849f8626
bubalopetar <bubalopetar95@gmail.com>
2023-02-25 18:45:24 +0100

    test

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _counter=0;
 
   void _incrementCounter() {
     setState(() {
@@ -37,6 +37,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    var a=2;
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
