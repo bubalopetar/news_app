763a2bf68bd0f322d0d6a1e6294214d4daf49df6
bubalopetar <bubalopetar95@gmail.com>
2023-02-27 17:13:26 +0100

    created ArticleRepositoryImpl class

---
+++ lib/features/articles/data/repositories/articles_repository_impl.dart
@@ -0,0 +1,22 @@
+import 'package:news_app/core/error/exceptions.dart';
+import 'package:news_app/features/articles/data/datasources/articles_remote_data_source.dart';
+import 'package:news_app/features/articles/data/models/article_model.dart';
+import 'package:news_app/core/error/failures.dart';
+import 'package:dartz/dartz.dart';
+import 'package:news_app/features/articles/domain/repositories/articles_repository.dart';
+
+class ArticleRepositoryImpl extends ArticlesRepository {
+  final ArticlesRemoteDataSource remoteDataSource;
+
+  ArticleRepositoryImpl({required this.remoteDataSource});
+  @override
+  Future<Either<Failure, List<ArticleModel>>> getArticlesFrom(
+      {required String url}) async {
+    try {
+      final response = await remoteDataSource.getArticlesFrom(url: url);
+      return Right(response);
+    } on ServerException {
+      return Left(ServerFailure());
+    }
+  }
+}
