8ba6657d4950add9567caa17f04dd017eb7d2353
bubalopetar <bubalopetar95@gmail.com>
2023-02-27 17:16:14 +0100

    created ArticlesXMLParserImpl class

---
+++ test/core/utils/articles_xml_parser_test.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:news_app/core/utils/articles_xml_parser.dart';
+import 'package:news_app/features/articles/data/models/article_model.dart';
+import 'package:xml/xml.dart';
+
+import '../../fixtures/fixture_reader.dart';
+
+void main() {
+  late ArticlesXMLParserImpl parser;
+  String xml = fixture('articles.xml');
+
+  group(
+    'getArticlesFromXML',
+    () {
+      setUp(
+        () {
+          parser = ArticlesXMLParserImpl();
+        },
+      );
+      test(
+        'should return a list of articles from provided xml string',
+        () {
+          final result = parser.getArticlesFromXML(xml);
+
+          expect(result.length, 150);
+          expect(result.first, isA<ArticleModel>());
+        },
+      );
+
+      test(
+        'should throw a XmlParserException if xml string is empty',
+        () {
+          expect(() => parser.getArticlesFromXML(''),
+              throwsA(isA<XmlParserException>()));
+        },
+      );
+    },
+  );
+}
