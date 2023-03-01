cfaac99010211f9b941927b4c48651c70d0fa85d
bubalopetar <bubalopetar95@gmail.com>
2023-03-01 22:46:23 +0100

    added articles event

---
+++ lib/features/articles/presentation/bloc/articles_event.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+part of 'articles_bloc.dart';
+
+abstract class ArticlesEvent extends Equatable {
+  const ArticlesEvent([List props = const <dynamic>[]]);
+
+  @override
+  List<Object> get props => [];
+}
+
+class GetArticlesFromUrlEvent extends ArticlesEvent {
+  final String url;
+  GetArticlesFromUrlEvent({
+    required this.url,
+  }) : super([url]);
+}
