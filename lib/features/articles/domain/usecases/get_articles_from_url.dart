7fa77ba1c537500e7e166e8c59bcaa2bd4c860dd
bubalopetar <bubalopetar95@gmail.com>
2023-03-01 22:45:19 +0100

    added use case

---
+++ lib/features/articles/domain/usecases/get_articles_from_url.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:news_app/core/error/failures.dart';
+import 'package:news_app/core/usecases/usecase.dart';
+import 'package:news_app/features/articles/domain/entities/article.dart';
+import 'package:news_app/features/articles/domain/repositories/articles_repository.dart';
+
+class GetArticlesUseCase implements UseCase<List<Article>, Params> {
+  final ArticlesRepository repository;
+  GetArticlesUseCase({
+    required this.repository,
+  });
+
+  @override
+  Future<Either<Failure, List<Article>>> call({required Params params}) async {
+    return repository.getArticlesFrom(url: params.url);
+  }
+}
+
+class Params extends Equatable {
+  final Uri url;
+  const Params({
+    required this.url,
+  });
+
+  @override
+  List<Object> get props => [url];
+}
