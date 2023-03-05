f2da9b6e0bf605b6f963950b8ff204dc1d31180c
bubalopetar <bubalopetar95@gmail.com>
2023-03-05 12:39:19 +0100

    search img and date in many places

--- lib/core/utils/articles_xml_parser.dart
+++ lib/core/utils/articles_xml_parser.dart
@@ -3,22 +3,22 @@ import 'package:news_app/features/articles/data/models/article_model.dart';
 import 'package:xml/xml.dart';
 
 abstract class ArticlesXMLParser {
-  late RegExp urlMatchingRegex;
   List<ArticleModel> getArticlesFromXML(String xml);
 }
 
 class ArticlesXMLParserImpl implements ArticlesXMLParser {
-  @override
+  late XmlDocument document;
+
   RegExp urlMatchingRegex = RegExp(
       r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)');
 
   @override
   List<ArticleModel> getArticlesFromXML(String xml) {
     List<ArticleModel> articles = [];
-    XmlDocument document = XmlDocument.parse(xml);
+    document = XmlDocument.parse(xml);
 
-    for (var articleXML in document.findAllElements('item')) {
-      dynamic result = _tryParseArticleModel(articleXML);
+    for (XmlElement articleXML in document.findAllElements('item')) {
+      dynamic result = _getArticleModelOrFalse(articleXML);
       if (result != false) {
         articles.add(result as ArticleModel);
       }
@@ -26,43 +26,61 @@ class ArticlesXMLParserImpl implements ArticlesXMLParser {
     return articles;
   }
 
-  dynamic _tryParseArticleModel(XmlElement article) {
+  dynamic _getArticleModelOrFalse(XmlElement article) {
     String? title = article.getElement('title')?.text;
     String? link = article.getElement('link')?.text;
     String? desc = article.getElement('description')?.text;
-    DateTime? pubDate = _getDateTime(article.getElement('pubDate')?.text);
-    String? img = _getImgLinkFromText(article.getElement('content')?.text);
+    String? category = article.getElement('category')?.text;
+    DateTime? pubDate = _tryToGetPubDate(article.getElement('pubDate')?.text);
+    String? img = _tryToGetImageLink(article);
 
     if (title != null && link != null) {
       return ArticleModel(
         title: title,
         link: link,
-        description: desc,
+        description: desc ?? '',
+        category: category ?? '',
         pubDate: pubDate,
-        img: img,
+        img: img ?? '',
       );
     } else {
       return false;
     }
   }
 
-  String? _getImgLinkFromText(String? text) {
+  String? _tryToGetImageLink(XmlElement article) {
+    String? img = _tryToGetLinkFromText(article.getElement('content')?.text);
+    if (img != null) return img;
+
+    img = article.getElement('media:thumbnail')!.getAttribute('url');
+    if (img != null) return img;
+
+    return null;
+  }
+
+  String? _tryToGetLinkFromText(String? text) {
     String? imgLink;
+
     if (text != null) {
       final match = urlMatchingRegex.firstMatch(text);
       if (match != null) {
-        String url = text.substring(match.start, match.end);
-        if (url.contains('.jpg')) {
-          imgLink = url;
-        }
+        imgLink = text.substring(match.start, match.end);
       }
     }
     return imgLink;
   }
 
-  DateTime? _getDateTime(String? dateString) {
-    return dateString != null
-        ? DateFormat('EEE, d MMM y hh:mm').parse(dateString)
-        : null;
+  DateTime? _tryToGetPubDate(String? dateString) {
+    if (dateString == null) {
+      return null;
+    }
+
+    DateTime? date;
+    try {
+      date = DateFormat('EEE, d MMM y hh:mm').parse(dateString);
+    } on FormatException {
+      date = DateFormat('y-d-MThh:mm').parse(dateString);
+    }
+    return date;
   }
 }
