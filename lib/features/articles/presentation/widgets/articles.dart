8ddfa8b8c068fcf6bf74b78863dc214b16e9d321
bubalopetar <bubalopetar95@gmail.com>
2023-03-05 10:23:23 +0100

    added Articles widget

---
+++ lib/features/articles/presentation/widgets/articles.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/features/articles/presentation/widgets/article.dart';
+
+import '../../domain/entities/article.dart';
+
+class Articles extends StatelessWidget {
+  const Articles({
+    Key? key,
+    required this.articles,
+  }) : super(key: key);
+
+  final List<Article> articles;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 5),
+      child: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: articles.length,
+              itemBuilder: (context, index) {
+                Article article = articles[index];
+                return ArticleWidget(article: article);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
