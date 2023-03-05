becde6b30a7fe93b0c6a41ecc65d1478e7fd8b2d
bubalopetar <bubalopetar95@gmail.com>
2023-03-05 10:18:06 +0100

    changed to regular class not abstract

--- lib/core/utils/url_converter.dart
+++ lib/core/utils/url_converter.dart
@@ -1,13 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:news_app/core/error/failures.dart';
 
-abstract class UrlConverter {
-  Either<Failure, Uri> toURI(String url);
-}
-
-class UrlConverterImpl implements UrlConverter {
-  UrlConverterImpl();
-  @override
+class UrlConverter {
   Either<InvalidUrlFailure, Uri> toURI(String url) {
     try {
       if (url.isEmpty) {
