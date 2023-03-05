a5e65ae5d12ab0a2b4df5809e0fad39af6a00a94
bubalopetar <bubalopetar95@gmail.com>
2023-03-05 10:20:03 +0100

    added category and changed var types

--- lib/features/articles/domain/entities/article.dart
+++ lib/features/articles/domain/entities/article.dart
@@ -4,15 +4,17 @@ import 'package:equatable/equatable.dart';
 class Article extends Equatable {
   final String title;
   final String link;
-  final String? description;
-  final String? img;
+  final String description;
+  final String img;
   final DateTime? pubDate;
+  final String category;
 
   const Article({
     required this.title,
     required this.link,
-    this.description,
-    this.img,
+    this.description = '',
+    this.img = '',
+    this.category = '',
     this.pubDate,
   });
 
