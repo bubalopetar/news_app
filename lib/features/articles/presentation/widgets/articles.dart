cce7de7b245dd03553ecdd3346b14b6f0558689b
bubalopetar <bubalopetar95@gmail.com>
2023-03-09 10:08:38 +0100

    added groupedListView

--- lib/features/articles/presentation/widgets/articles.dart
+++ lib/features/articles/presentation/widgets/articles.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grouped_list/grouped_list.dart';
 import 'package:news_app/features/articles/presentation/widgets/article.dart';
 
 import '../../domain/entities/article.dart';
@@ -13,23 +14,49 @@ class Articles extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 5,
-      ),
-      child: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: articles.length,
-              itemBuilder: (context, index) {
-                Article article = articles[index];
-                return ArticleWidget(article: article);
-              },
-            ),
+    return Column(
+      children: [
+        Expanded(
+          child: GroupedListView(
+            useStickyGroupSeparators: true,
+            order: GroupedListOrder.DESC,
+            elements: articles,
+            groupSeparatorBuilder: (String category) => Category(category),
+            groupBy: (element) => element.category,
+            itemBuilder: (context, Article article) {
+              return ArticleWidget(article: article);
+            },
           ),
-        ],
-      ),
+        ),
+      ],
+    );
+  }
+}
+
+class Category extends StatelessWidget {
+  final Color tileColor = Colors.grey.withOpacity(0.5);
+  final String category;
+  Category(
+    this.category, {
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 1),
+            decoration: BoxDecoration(
+                color: tileColor,
+                border: Border.all(color: tileColor),
+                borderRadius: const BorderRadius.all(Radius.circular(10))),
+            child: Text(
+              category,
+              style: const TextStyle(fontSize: 16, color: Colors.white),
+            ))
+      ],
     );
   }
 }
