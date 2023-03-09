8446dec545bf22d35c286f4df0dfefe2bf91ff64
bubalopetar <bubalopetar95@gmail.com>
2023-03-09 10:08:20 +0100

    refactor

--- lib/features/articles/presentation/widgets/article.dart
+++ lib/features/articles/presentation/widgets/article.dart
@@ -4,6 +4,7 @@ import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:news_app/injection_container.dart';
 
 import '../../domain/entities/article.dart';
+import '../pages/article_web_view.dart';
 
 class ArticleWidget extends StatelessWidget {
   const ArticleWidget({
@@ -11,26 +12,40 @@ class ArticleWidget extends StatelessWidget {
     required this.article,
   }) : super(key: key);
 
+  AspectRatio? _getLeading() {
+    return article.img == ''
+        ? null
+        : AspectRatio(
+            aspectRatio: 16 / 9,
+            child: CachedNetworkImage(
+              fit: BoxFit.cover,
+              imageUrl: article.img,
+              cacheManager: serviceLocator.get<CacheManager>(),
+              placeholder: (context, url) =>
+                  const Center(child: CircularProgressIndicator()),
+              errorWidget: (context, url, error) => const Icon(Icons.error),
+            ),
+          );
+  }
+
   final Article article;
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      contentPadding: const EdgeInsets.only(top: 5),
-      visualDensity: const VisualDensity(vertical: 4),
-      title: Text(article.title),
-      subtitle: Text(article.category),
-      leading: article.img == ''
-          ? null
-          : AspectRatio(
-              aspectRatio: 16 / 9,
-              child: CachedNetworkImage(
-                imageUrl: article.img,
-                cacheManager: serviceLocator.get<CacheManager>(),
-                placeholder: (context, url) =>
-                    const Center(child: CircularProgressIndicator()),
-                errorWidget: (context, url, error) => const Icon(Icons.error),
-              ),
-            ),
+    return InkWell(
+      onTap: () => Navigator.of(context)
+          .pushNamed(WebViewPage.routeName, arguments: article.link),
+      child: Card(
+        elevation: 5,
+        child: ListTile(
+          visualDensity: const VisualDensity(vertical: 4),
+          contentPadding: const EdgeInsets.all(0),
+          title: Text(
+            article.title,
+            maxLines: 3,
+          ),
+          leading: _getLeading(),
+        ),
+      ),
     );
   }
 }
