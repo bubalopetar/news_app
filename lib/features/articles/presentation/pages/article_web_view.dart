2f28b27b8d4d599a9ceee5658a07d53a991d57dc
bubalopetar <bubalopetar95@gmail.com>
2023-03-09 10:06:28 +0100

    added webview page

---
+++ lib/features/articles/presentation/pages/article_web_view.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewPage extends StatefulWidget {
+  static String routeName = 'articleWebView';
+  const WebViewPage({super.key});
+
+  @override
+  State<WebViewPage> createState() => _WebViewPageState();
+}
+
+class _WebViewPageState extends State<WebViewPage> {
+  bool init = true;
+  late final WebViewController controller;
+  late final String link;
+
+  @override
+  void didChangeDependencies() {
+    if (init) {
+      link = ModalRoute.of(context)!.settings.arguments as String;
+      controller.loadRequest(
+        Uri.parse(link),
+      );
+      init = !init;
+    }
+    super.didChangeDependencies();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: const ClosePage(),
+      body: WebViewWidget(
+        controller: controller,
+      ),
+    );
+  }
+}
+
+class ClosePage extends StatelessWidget {
+  const ClosePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      elevation: 0,
+      backgroundColor: Colors.red.withOpacity(0.5),
+      child: const Icon(Icons.close),
+      onPressed: () => Navigator.of(context).pop(),
+    );
+  }
+}
