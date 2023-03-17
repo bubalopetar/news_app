7df97fd4a613ea0d01bb88916505757f170b5457
bubalopetar <bubalopetar95@gmail.com>
2023-03-17 16:34:17 +0100

    added localDataSourceMock and test group

--- test/features/articles/data/repository/articles_repository_impl_test.dart
+++ test/features/articles/data/repository/articles_repository_impl_test.dart
@@ -5,28 +5,40 @@ import 'package:mockito/annotations.dart';
 import 'package:news_app/core/error/exceptions.dart';
 import 'package:news_app/core/error/failures.dart';
 import 'package:news_app/core/utils/articles_xml_parser.dart';
+import 'package:news_app/features/articles/data/datasources/articles_local_data_source.dart';
 import 'package:news_app/features/articles/data/datasources/articles_remote_data_source.dart';
 import 'package:news_app/features/articles/data/models/article_model.dart';
 import 'package:news_app/features/articles/data/repositories/articles_repository_impl.dart';
 
 import '../../../../fixtures/fixture_reader.dart';
-@GenerateNiceMocks([MockSpec<ArticlesRemoteDataSource>()])
+@GenerateNiceMocks(
+    [MockSpec<ArticlesRemoteDataSource>(), MockSpec<ArticlesLocalDataSource>()])
 import 'articles_repository_impl_test.mocks.dart';
 
 void main() {
-  MockArticlesRemoteDataSource mockArticlesRemoteDataSource =
+  MockArticlesRemoteDataSource articlesRemoteDataSource =
       MockArticlesRemoteDataSource();
-  ArticlesRepositoryImpl repository =
-      ArticlesRepositoryImpl(remoteDataSource: mockArticlesRemoteDataSource);
+
+  MockArticlesLocalDataSource articlesLocalDataSource =
+      MockArticlesLocalDataSource();
+
+  ArticlesRepositoryImpl repository = ArticlesRepositoryImpl(
+      remoteDataSource: articlesRemoteDataSource,
+      localDataSource: articlesLocalDataSource);
+
   Uri url = Uri.parse('testUrl');
   List<ArticleModel> articles =
       ArticlesXMLParserImpl().getArticlesFromXML(fixture('articles.xml'));
 
+  ArticleModel article = const ArticleModel(title: 'title', link: 'link');
+
   setUp(
     () {
-      mockArticlesRemoteDataSource = MockArticlesRemoteDataSource();
+      articlesLocalDataSource = MockArticlesLocalDataSource();
+      articlesRemoteDataSource = MockArticlesRemoteDataSource();
       repository = ArticlesRepositoryImpl(
-          remoteDataSource: mockArticlesRemoteDataSource);
+          remoteDataSource: articlesRemoteDataSource,
+          localDataSource: articlesLocalDataSource);
     },
   );
   group(
@@ -35,10 +47,10 @@ void main() {
       test(
         'should call datasource.getArticlesFrom and get articles',
         () async {
-          when(mockArticlesRemoteDataSource.getArticlesFrom(url: url))
+          when(articlesRemoteDataSource.getArticlesFrom(url: url))
               .thenAnswer((realInvocation) async => articles);
           final response = await repository.getArticlesFrom(url: url);
-          verify(mockArticlesRemoteDataSource.getArticlesFrom(url: url));
+          verify(articlesRemoteDataSource.getArticlesFrom(url: url));
           expect(response, Right(articles));
         },
       );
@@ -46,12 +58,75 @@ void main() {
       test(
         'should call datasource.getArticlesFrom and get failure if server error',
         () async {
-          when(mockArticlesRemoteDataSource.getArticlesFrom(url: url))
+          when(articlesRemoteDataSource.getArticlesFrom(url: url))
               .thenThrow(ServerException());
 
           final response = await repository.getArticlesFrom(url: url);
-          verify(mockArticlesRemoteDataSource.getArticlesFrom(url: url));
+          verify(articlesRemoteDataSource.getArticlesFrom(url: url));
           expect(response, Left(ServerFailure()));
+        },
+      );
+    },
+  );
+
+  group(
+    "cacheArticle",
+    () {
+      test(
+        "should call localDataSource cacheArticleFunction and return true",
+        () async {
+          when(articlesLocalDataSource.cacheArticle(article))
+              .thenAnswer((realInvocation) => Future.value(true));
+          final response = await repository.setToFavorites(article);
+
+          verify(articlesLocalDataSource.cacheArticle(article));
+          expect(response, const Right(true));
+        },
+      );
+
+      test(
+        "should return CacheFailure when localDataSource throws an CacheException",
+        () async {
+          when(articlesLocalDataSource.cacheArticle(article))
+              .thenThrow(CacheException());
+          final response = await repository.setToFavorites(article);
+
+          verify(articlesLocalDataSource.cacheArticle(article));
+          expect(response, Left(CacheFailure()));
+        },
+      );
+    },
+  );
+
+  group(
+    "getFavorites",
+    () {
+      test(
+        "should get and return a list of articles from datasource",
+        () async {
+          when(articlesLocalDataSource.getCachedArticles())
+              .thenAnswer((realInvocation) => articles);
+          final result = repository.getFavorites();
+          expect(result, right(articles));
+        },
+      );
+      test(
+        "should get and return a null from datasource",
+        () async {
+          when(articlesLocalDataSource.getCachedArticles())
+              .thenAnswer((realInvocation) => null);
+          final result = repository.getFavorites();
+          expect(result, right(null));
+        },
+      );
+      test(
+        "should catch and CacheException and return CacheFailure",
+        () async {
+          when(articlesLocalDataSource.getCachedArticles())
+              .thenThrow(CacheException());
+
+          final result = repository.getFavorites();
+          expect(result, Left(CacheFailure()));
         },
       );
     },
