a09603536321b2152f8e9443f941dbed8faef322
bubalopetar <bubalopetar95@gmail.com>
2023-03-17 16:30:52 +0100

    added 2 methods

--- lib/features/articles/domain/repositories/articles_repository.dart
+++ lib/features/articles/domain/repositories/articles_repository.dart
@@ -4,4 +4,6 @@ import 'package:news_app/features/articles/domain/entities/article.dart';
 
 abstract class ArticlesRepository {
   Future<Either<Failure, List<Article>>> getArticlesFrom({required Uri url});
+  Future<Either<Failure, bool>> setToFavorites(Article article);
+  Either<Failure, List<Article>?> getFavorites();
 }
