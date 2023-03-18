02b2b44996c5c5200c069d5e71fb6c01bbb9ebd2
bubalopetar <bubalopetar95@gmail.com>
2023-03-18 13:48:19 +0100

    added multiple events and handlers

--- lib/features/articles/presentation/bloc/articles_bloc.dart
+++ lib/features/articles/presentation/bloc/articles_bloc.dart
@@ -5,7 +5,9 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:news_app/core/utils/url_converter.dart';
 import 'package:news_app/features/articles/domain/entities/article.dart';
+import 'package:news_app/features/articles/domain/usecases/toggle_favorites.dart';
 import 'package:news_app/features/articles/domain/usecases/get_articles_from_url.dart';
+import 'package:news_app/features/articles/domain/usecases/get_favorites.dart';
 
 part 'articles_event.dart';
 part 'articles_state.dart';
@@ -14,11 +16,22 @@ const serverFailureMessage = 'SERVER_FAILURE_MESSAGE';
 const invalidUrlFailureMessage = 'INVALID_URL_FAILURE';
 
 class ArticlesBloc extends Bloc<ArticlesEvent, ArticlesState> {
-  GetArticlesUseCase usecase;
+  GetArticlesUseCase getArticlesUseCase;
+  ToggleFavoritesUseCase addToFavoritesUseCase;
+  GetFavoritesUseCase getFavoritesUseCase;
   UrlConverter urlConverter;
-  ArticlesBloc({required this.usecase, required this.urlConverter})
+  ArticlesBloc(
+      {required this.getArticlesUseCase,
+      required this.addToFavoritesUseCase,
+      required this.getFavoritesUseCase,
+      required this.urlConverter})
       : super(const Empty()) {
     on<GetArticlesFromUrlEvent>(getArticlesFromUrlEventHandler);
+    on<TogleFavoritesEvent>(toggleFavoritesEventHandler);
+  }
+
+  void toggleFavoritesEventHandler(event, emit) async {
+    addToFavoritesUseCase(params: CAParams(article: event.article));
   }
 
   void getArticlesFromUrlEventHandler(event, emit) async {
@@ -30,18 +43,27 @@ class ArticlesBloc extends Bloc<ArticlesEvent, ArticlesState> {
           activeTabIndex: event.activeTabIndex));
     }, (url) async {
       emit(Loading(activeTabIndex: event.activeTabIndex));
-      await _callUseCase(event, url, emit);
+      await _callGetArticlesUseCase(event, url, emit);
     });
   }
 
-  FutureOr<void> _callUseCase(event, url, emit) async {
-    final articles = await usecase(params: Params(url: url));
+  List<Article> getFavorites() {
+    final r = getFavoritesUseCase.syncCall(params: NoParams());
+    return r;
+  }
+
+  FutureOr<void> _callGetArticlesUseCase(event, url, emit) async {
+    final articles = await getArticlesUseCase(params: Params(url: url));
+    final favorites = getFavorites();
 
     await articles.fold((failure) async {
       emit(Error(
           message: serverFailureMessage, activeTabIndex: event.activeTabIndex));
     }, (articles) async {
-      emit(Loaded(articles: articles, activeTabIndex: event.activeTabIndex));
+      emit(Loaded(
+          articles: articles,
+          activeTabIndex: event.activeTabIndex,
+          favorites: favorites));
     });
   }
 }
