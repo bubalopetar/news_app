4c7ff1f3d2943d86532bdeb65e49472c4fcd03d0
bubalopetar <bubalopetar95@gmail.com>
2023-03-18 13:49:53 +0100

    convert to statefull and add favorite button

--- lib/features/articles/presentation/widgets/article.dart
+++ lib/features/articles/presentation/widgets/article.dart
@@ -1,25 +1,56 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:news_app/injection_container.dart';
 
 import '../../domain/entities/article.dart';
+import '../bloc/articles_bloc.dart';
 import '../pages/article_web_view.dart';
 
-class ArticleWidget extends StatelessWidget {
+class ArticleWidget extends StatefulWidget {
   const ArticleWidget({
-    Key? key,
+    required Key? key,
     required this.article,
+    required this.favorites,
   }) : super(key: key);
 
+  final List<Article> favorites;
+  final Article article;
+
+  @override
+  State<ArticleWidget> createState() => _ArticleWidgetState();
+}
+
+class _ArticleWidgetState extends State<ArticleWidget> {
+  bool init = true;
+  bool isFavorite = true;
+
+  @override
+  void initState() {
+    if (init) {
+      init = false;
+      isFavorite = widget.favorites.contains(widget.article);
+    }
+    super.initState();
+  }
+
+  void toggleFavorites(Article article, BuildContext context) {
+    setState(() {
+      isFavorite = !isFavorite;
+    });
+
+    BlocProvider.of<ArticlesBloc>(context).add(TogleFavoritesEvent(article));
+  }
+
   AspectRatio? _getLeading() {
-    return article.img == ''
+    return widget.article.img == ''
         ? null
         : AspectRatio(
             aspectRatio: 16 / 9,
             child: CachedNetworkImage(
               fit: BoxFit.cover,
-              imageUrl: article.img,
+              imageUrl: widget.article.img,
               cacheManager: serviceLocator.get<CacheManager>(),
               placeholder: (context, url) =>
                   const Center(child: CircularProgressIndicator()),
@@ -28,22 +59,32 @@ class ArticleWidget extends StatelessWidget {
           );
   }
 
-  final Article article;
+  Icon getFavoriteIcon() {
+    if (isFavorite) {
+      return const Icon(Icons.favorite);
+    } else {
+      return const Icon(Icons.favorite_border);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () => Navigator.of(context)
-          .pushNamed(WebViewPage.routeName, arguments: article.link),
+          .pushNamed(WebViewPage.routeName, arguments: widget.article.link),
       child: Card(
         elevation: 5,
         child: ListTile(
           visualDensity: const VisualDensity(vertical: 4),
           contentPadding: const EdgeInsets.all(0),
           title: Text(
-            article.title,
+            widget.article.title,
             maxLines: 3,
           ),
           leading: _getLeading(),
+          trailing: IconButton(
+              onPressed: () => toggleFavorites(widget.article, context),
+              icon: getFavoriteIcon()),
         ),
       ),
     );
