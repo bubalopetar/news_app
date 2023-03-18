9e8aa406a87ec59d0f1cb6ec0b7afcd17eced7bf
bubalopetar <bubalopetar95@gmail.com>
2023-03-18 17:31:52 +0100

    convert to statefull

--- lib/features/articles/presentation/widgets/sources_navigation_bar.dart
+++ lib/features/articles/presentation/widgets/sources_navigation_bar.dart
@@ -4,15 +4,30 @@ import 'package:news_app/sources.dart';
 
 import '../bloc/articles_bloc.dart';
 
-class SourcesNavigationBar extends StatelessWidget {
+class SourcesNavigationBar extends StatefulWidget {
   final int activeTabIndex;
 
   const SourcesNavigationBar({super.key, required this.activeTabIndex});
 
+  @override
+  State<SourcesNavigationBar> createState() => _SourcesNavigationBarState();
+}
+
+class _SourcesNavigationBarState extends State<SourcesNavigationBar> {
+  final int optionsIndex = 6;
+
+  BottomNavigationBarItem getOptionsItem() {
+    return const BottomNavigationBarItem(
+        icon: Icon(
+          Icons.more_vert,
+        ),
+        label: '');
+  }
+
   List<BottomNavigationBarItem> buildBottomNavigationItems() {
-    return sources
+    List<BottomNavigationBarItem> items = sources
         .map<BottomNavigationBarItem>((source) => BottomNavigationBarItem(
-              backgroundColor: Colors.grey.shade500,
+              backgroundColor: Theme.of(context).bottomAppBarColor,
               label: source.name,
               icon: Image(
                 image: AssetImage("assets/images/${source.name}.png"),
@@ -21,9 +36,36 @@ class SourcesNavigationBar extends StatelessWidget {
               ),
             ))
         .toList();
+    items.add(getOptionsItem());
+    return items;
+  }
+
+  showOptionsMenu(context) async {
+    final items = [
+      const PopupMenuItem(
+        value: Options.favorites,
+        child: Text("Favorites"),
+      ),
+    ];
+    final size = MediaQuery.of(context).size;
+    final width = size.width;
+    final height = size.height;
+    final bottomPosition = height - 65 - (items.length * 50);
+    final selected = await showMenu(
+        context: context,
+        position: RelativeRect.fromLTRB(width, bottomPosition, 0, 0),
+        items: items);
+    if (selected == Options.favorites) {
+    } else {
+      return;
+    }
   }
 
   void _onSelectedNavigationItem(int index, BuildContext context) {
+    if (index == optionsIndex) {
+      showOptionsMenu(context);
+      return;
+    }
     var url = sources[index].url;
     BlocProvider.of<ArticlesBloc>(context)
         .add(GetArticlesFromUrlEvent(url: url, activeTabIndex: index));
@@ -32,9 +74,11 @@ class SourcesNavigationBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      currentIndex: activeTabIndex,
+      currentIndex: widget.activeTabIndex,
       onTap: (index) => _onSelectedNavigationItem(index, context),
       items: buildBottomNavigationItems(),
     );
   }
 }
+
+enum Options { favorites }
