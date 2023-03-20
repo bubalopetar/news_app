df2e1d4f53091a07b07670678868aec27d7caedc
bubalopetar <bubalopetar95@gmail.com>
2023-03-20 19:05:47 +0100

    added theme data

---
+++ lib/core/themes/app_themes.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+enum AppTheme { dark, light }
+
+final appThemes = {
+  AppTheme.light: ThemeData.light().copyWith(
+      primaryColor: Colors.white,
+      backgroundColor: Colors.white,
+      bottomAppBarColor: Colors.grey.shade500,
+      floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.red.withOpacity(0.5))),
+  AppTheme.dark: ThemeData.dark().copyWith(
+    primaryColor: Colors.white,
+    backgroundColor: Colors.grey.shade800,
+    bottomAppBarColor: Colors.grey.shade800,
+    bottomNavigationBarTheme:
+        const BottomNavigationBarThemeData(selectedItemColor: Colors.white),
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+        backgroundColor: Colors.red.withOpacity(0.5)),
+    iconTheme: IconThemeData(color: Colors.red.shade400),
+  ),
+};
