caef0542cadb7d256945346ffb3ce013fccf1669
bubalopetar <bubalopetar95@gmail.com>
2023-03-20 19:06:24 +0100

    changed call return value to type

--- lib/core/usecases/usecase.dart
+++ lib/core/usecases/usecase.dart
@@ -1,7 +1,3 @@
-import 'package:dartz/dartz.dart';
-
-import '../error/failures.dart';
-
 abstract class UseCase<Type, Params> {
-  Future<Either<Failure, Type>> call({required Params params});
+  Type call({required Params params});
 }
