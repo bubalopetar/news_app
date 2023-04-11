eb39ac090b146d8ba950ba1ea907f73743aa782d
bubalopetar <bubalopetar95@gmail.com>
2023-04-11 11:13:24 +0200

    revert changes

--- lib/core/themes/app_themes.dart
+++ lib/core/themes/app_themes.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 
 enum AppTheme { dark, light }
@@ -5,20 +7,18 @@ enum AppTheme { dark, light }
 final appThemes = {
   AppTheme.light: ThemeData.light().copyWith(
       primaryColor: Colors.white,
+      backgroundColor: Colors.white,
+      bottomAppBarColor: Colors.grey.shade500,
       floatingActionButtonTheme: FloatingActionButtonThemeData(
-          backgroundColor: Colors.red.withOpacity(0.5)),
-      bottomAppBarTheme: BottomAppBarTheme(color: Colors.grey.shade500),
-      colorScheme:
-          const ColorScheme.light().copyWith(background: Colors.white)),
+          backgroundColor: Colors.red.withOpacity(0.5))),
   AppTheme.dark: ThemeData.dark().copyWith(
     primaryColor: Colors.white,
+    backgroundColor: Colors.grey.shade800,
+    bottomAppBarColor: Colors.grey.shade800,
     bottomNavigationBarTheme:
         const BottomNavigationBarThemeData(selectedItemColor: Colors.white),
     floatingActionButtonTheme: FloatingActionButtonThemeData(
         backgroundColor: Colors.red.withOpacity(0.5)),
     iconTheme: IconThemeData(color: Colors.red.shade400),
-    bottomAppBarTheme: BottomAppBarTheme(color: Colors.grey.shade800),
-    colorScheme:
-        const ColorScheme.light().copyWith(background: Colors.grey.shade800),
   ),
 };
